6f399c48a6dae42a88e2eef93b6e3e560453bbd8
kristjantagel <kristjan.tagel@gmail.com>
2024-01-18 23:15:21 +0200

    commit

--- lõputöö/express_server/server.js
+++ lõputöö/express_server/server.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const app = express();
 const port = 3002;
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public', 'views'));
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
